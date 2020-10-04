e48121a14e5ce3a9b0f7fcfb8e327d25410e85b2
fabricio cintra <fabricioalvescintra@gmail.com>
2020-10-04 17:30:00 -0300

    aula de javascript
    1

---
+++ js/main.js
@@ -0,0 +1,57 @@
+/*
+var nome = "fabricio";
+var n1 = 6;
+var n2 = 10;
+var frase = "japao e o melhor time do mundo";
+console.log(n1 + n2);
+console.log(frase.replace("japao","brasil"));
+*/
+
+/* var lista = [
+   "maca","pera","laranja"
+];
+lista.push("uva");//adiciona 
+lista.pop();//tira ultimo elemento da lista
+console.log(lista.toString());
+console.log(lista.join("***"));
+
+var fruta = {nome:"Maca", cor:"vermelha"}
+console.log(fruta); */
+
+/*var frutas = [
+    {
+        nome:"maca", cor:"vermelha"
+    },
+    {
+        nome:"uva", cor:"roxa"
+    }
+]
+console.log(frutas[1]);
+*/
+
+/*var idade = prompt("qual sua idade");
+if(idade >=18){
+    console.log("maior de idade");
+}
+else{
+    console.log("menor de idade");
+}
+*/
+
+/*
+var c = 0;
+while(c <=5){
+    console.log(c);
+    c++;
+};
+*/
+
+/* var c;
+for(c=0; c<=5; c++){
+    alert(c);
+} */
+
+/* var d = new Date();
+alert( d.getHours() );
+alert(d.getMinutes()); 
+*/
